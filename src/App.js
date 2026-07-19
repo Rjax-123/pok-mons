43e88193e5f73e5d044e5bb7ababb0e65d6d8af8
Rjax-123 <i25c.2425@gmail.com>
2026-07-20 00:03:52 +0300

    Refactor App component to use new structurea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import HeaderSection from "./components/HeaderSection";
+import ProductList from "./components/ProductList";
+import SimilarOffers from "./components/SimilarOffers";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
+import siteData from "./data/siteData.json";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="lma-app">
+      <Navbar />
+      <main className="lma-main">
+        <HeaderSection />
+        <ProductList products={siteData.products} />
+        <SimilarOffers offers={siteData.similar} />
+        <Newsletter />
+      </main>
+      <Footer />
     </div>
   );
 }
